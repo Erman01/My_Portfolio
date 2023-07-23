04c72c56039fb31ca752a0f347fc44677611095c
Erman01 <ekemanci@gmail.com>
2023-07-23 06:17:46 +0100

    Emailjs added to contact form

--- index.js
+++ index.js
@@ -26,4 +26,61 @@ function closeMenu() {
 
 /* --------------Contact form */
 
-  /* -----------End of Contact Form-------------- */
+// function sendMail() {
+//   var tempParams = {
+//     name: document.getElementById("name").value,
+//      email: document.getElementById("email").value,
+//     subject: document.getElementById("subject").value,
+//     message: document.getElementById("message").value
+//   };
+//   const serviceID = "service_d3ekv9c";
+//   const templateID = "template_x58txm5";
+//   emailjs.send(serviceID, templateID, tempParams).then((res) => {
+//       document.getElementById("name").value = "";
+//       document.getElementById("email").value = "";
+//       document.getElementById("subject").value = "";
+//       document.getElementById("message").value = "";
+//       alert("your message sent");
+//     }).catch((err) => console.log(err));
+//   }
+const contactForm = document.querySelector("#contact-form");
+const submitButton = document.querySelector(".submit-btn");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+// const subjectInput = document.querySelector("#subject");
+const messageInput = document.querySelector("#message");
+
+const publicKey = "0UruFJZGzE6WDIGRJ";
+const serviceID = "service_d3ekv9c";
+const templateID = "template_x58txm5";
+
+emailjs.init(publicKey);
+contactForm.addEventListener("submit", e => {
+  e.preventDefault();
+  submitButton.innerText = "Just A Moment...";
+  const inputFields = {
+    name: nameInput.value,
+    email: emailInput.value,
+    // subject: subjectInput.value,
+    message:messageInput.value
+  }
+  emailjs.send(serviceID, templateID, inputFields)
+    .then(() => {
+      submitButton.innerHTML = "Message Sent Successfully";
+      nameInput.value = "";
+      emailInput.value = "";
+      // subjectInput.value = "";
+      messageInput.value = "";
+     
+    }, (error) => {
+      console.log(error);
+      submitButton.innerText="Something went wrong"
+    })
+  
+})
+/* -----------End of Contact Form-------------- */
+  
+/* Scroll Effect */
+
+    // Get the current page scroll position
+   
